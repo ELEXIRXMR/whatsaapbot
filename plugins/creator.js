3b4414c958e3ea21d75cf5b98f86f68ea23f6563
ELEXIRXMR <74086780+ELEXIRXMR@users.noreply.github.com>
2021-02-20 09:47:02 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
+  // this.sendContact(m.chat, '6283144574743', 'Maha', m)
   this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
 }
 handler.help = ['owner', 'creator']
